99adadc846cc64eb72408522dc8138b1cb6afb5e
Donald Blanc <dxb3662@rit.edu>
2018-11-08 19:55:00 -0500

    challege solution

---
+++ sport.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+#challenge 
+# Create a script named sport.sh
+#make is executable
+#accept two parameters : a name and a favorite sport
+#display any sentence to the coneole using those imports
+
+NAME=$1
+FAV_SPORT=$2
+echo Hi $NAME your favorite sport is $FAV_SPORT
+exit 0
